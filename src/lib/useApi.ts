1617828f285d061d1d6b60cc8d759fb565794844
eyemono-moe <ysdr83@gmail.com>
2023-12-06 21:54:23 +0900

    :sparkles: display traP name

---
+++ src/lib/useApi.ts
@@ -0,0 +1,18 @@
+import { Apis, Configuration } from "@traptitech/traq";
+import { redirect } from "solid-start";
+import { session } from "./session";
+
+const useApi = async (request: Request) => {
+	const sessionData = await session.getSessionData(request);
+	if (!sessionData) {
+		throw redirect("/login");
+	}
+	const api = new Apis(
+		new Configuration({
+			accessToken: sessionData.token,
+		}),
+	);
+	return api;
+};
+
+export default useApi;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,31 @@
-export default function Index() {
+import { createEffect } from "solid-js";
+import { useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
+import useApi from "~/lib/useApi";
+
+export const routeData = () => {
+	const me = createServerData$(async (_, event) => {
+		console.log("routeData me");
+		const api = await useApi(event.request);
+		const { data: me } = await api.getMe();
+		return me;
+	});
+
+	return { me };
+};
+
+export default function Page() {
+	console.log("Page");
+	const { me } = useRouteData<typeof routeData>();
+
+	createEffect(() => {
+		console.log(me());
+	});
+
 	return (
 		<main>
 			<h1>Hello world!</h1>
+			<div>{me()?.displayName}</div>
 		</main>
 	);
 }
